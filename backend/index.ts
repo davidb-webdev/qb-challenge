0fa5a6a3dfdd0f9a2a1bef607cf46302fe7a366d
Rikard Johansson <rikard.johansson@quickbutik.com>
2025-06-30 16:29:23 +0200

    Adding instructions

--- backend/index.ts
+++ backend/index.ts
@@ -11,7 +11,7 @@ const dbConfig = {
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'root',
   password: process.env.DB_PASSWORD || 'password',
-  database: process.env.DB_NAME ,
+  database: process.env.DB_NAME,
   port: parseInt(process.env.DB_PORT || '3306')
 };
 
