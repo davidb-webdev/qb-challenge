9e587c448da6c601d5579f47d6a3eb1458cf40db
David Boström <144777770+davidb-webdev@users.noreply.github.com>
2025-09-03 17:18:48 +0200

    re-style ProductNameFilter

--- frontend/src/components/ProductGrid/ProductNameFilter.tsx
+++ frontend/src/components/ProductGrid/ProductNameFilter.tsx
@@ -8,14 +8,22 @@ export default function ProductNameFilter({
   setNameFilter,
 }: ProductNameFilterProps) {
   return (
-    <div className="flex gap-3 items-center">
-      <label htmlFor="productNameFilter">Filter product name:</label>
+    <div className="flex gap-3 items-center relative">
+      <label
+        htmlFor="productNameFilter"
+        className={
+          'absolute text-black p-3' +
+          (nameFilter.length > 0 ? ' sr-only' : '')
+        }
+      >
+        Product name
+      </label>
       <input
-				id="productNameFilter"
+        id="productNameFilter"
         type="search"
         value={nameFilter}
         onChange={(event) => setNameFilter(event.target.value)}
-        className="rounded-lg text-black px-2 border-2 outline-none focus:border-blurple font-bold transition-colors"
+        className="rounded-3xl text-black px-3 py-1 border-2 outline-none focus:border-blurple transition-colors"
       />
     </div>
   )
