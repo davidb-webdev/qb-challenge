71c5496daa2dffc3bab797a29b08dac91988bf3d
David Boström <144777770+davidb-webdev@users.noreply.github.com>
2025-09-03 17:28:59 +0200

    prevent loading page multiple times + semantic touch-up

--- frontend/src/components/ProductGrid/ProductGrid.tsx
+++ frontend/src/components/ProductGrid/ProductGrid.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useCallback, useEffect, useRef, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { Product } from '@/types/Product'
 import ProductCard from './ProductCard'
 import ListOptions from './ListOptions'
@@ -29,8 +29,11 @@ export function ProductGrid() {
   const [error, setError] = useState(false)
   const [displayAsGrid, setDisplayAsGrid] = useState(true)
   const [nameFilter, setNameFilter] = useState<string>('')
-  const intersectionRef = useRef<HTMLDivElement | null>(null)
 
+  const observerElementRef = useRef<HTMLDivElement | null>(null)
+  const observerRef = useRef<IntersectionObserver | null>(null)
+
+  // Initial load
   useEffect(() => {
     fetchProducts()
   }, [])
@@ -38,21 +41,36 @@ export function ProductGrid() {
   // Infinity scroll pagination
   useEffect(() => {
     // Return early if...
-    if (!intersectionRef.current || loading || error || !pagination.hasNextPage)
+    if (
+      !observerElementRef.current ||
+      loading ||
+      error ||
+      !pagination.hasNextPage
+    )
       return
 
     // Fetch products if user scrolls to observer element
-    const intersectionObserver = new IntersectionObserver((entries) => {
-      if (entries[0].isIntersecting) fetchProducts(pagination.page + 1)
-    })
-    intersectionObserver.observe(intersectionRef.current)
+    const intersectionObserver = new IntersectionObserver(
+      (entries) => {
+        if (entries[0].isIntersecting) fetchProducts(pagination.page + 1)
+      },
+      { rootMargin: '300px 0px' },
+    )
+
+    intersectionObserver.observe(observerElementRef.current)
+    observerRef.current = intersectionObserver
 
     // Clean up observer on unmount
     return () => intersectionObserver.disconnect()
-  }, [pagination, error])
+  }, [pagination, loading, error])
 
   const fetchProducts = async (page: number = 1, limit: number = 12) => {
     setLoading(true)
+    // Pause observer while loading
+    if (observerRef.current && observerElementRef.current) {
+      observerRef.current.unobserve(observerElementRef.current)
+    }
+
     try {
       const response = await fetch(`/api/products?page=${page}&limit=${limit}`)
       const data = await response.json()
@@ -75,8 +93,8 @@ export function ProductGrid() {
       : products
 
   return (
-    <div>
-      <div className="flex gap-3 justify-between items-center pb-3">
+    <>
+      <section className="flex gap-3 justify-between items-center pb-3">
         <ProductNameFilter
           nameFilter={nameFilter}
           setNameFilter={setNameFilter}
@@ -85,42 +103,48 @@ export function ProductGrid() {
           displayAsGrid={displayAsGrid}
           setDisplayAsGrid={setDisplayAsGrid}
         />
-      </div>
+      </section>
 
-      <div
-        className={
-          'grid gap-3' +
-          (displayAsGrid
-            ? ' grid-cols-[repeat(auto-fit,minmax(14rem,1fr))]'
-            : ' grid-cols-1')
-        }
-      >
-        {filteredProducts.map((product) => (
-          <ProductCard
-            key={product.id}
-            product={product}
-            displayAsGrid={displayAsGrid}
-          />
-        ))}
-      </div>
-
-      {error && (
-        <div className="p-5 text-center">
-          <p>Couldn't load products</p>
-          <button
-            onClick={() => fetchProducts(pagination.page + 1)}
-            className="mt-3 p-3 px-5 bg-blue-950 hover:bg-blurple rounded-xl"
-          >
-            Retry
-          </button>
+      <section>
+        <div
+          className={
+            'grid gap-3' +
+            (displayAsGrid
+              ? ' grid-cols-[repeat(auto-fit,minmax(14rem,1fr))]'
+              : ' grid-cols-1')
+          }
+        >
+          {filteredProducts.map((product) => (
+            <ProductCard
+              key={product.id}
+              product={product}
+              displayAsGrid={displayAsGrid}
+            />
+          ))}
         </div>
-      )}
 
-      {!pagination.hasNextPage && !loading && !error && (
-        <p className="p-5 text-center">Displaying all products</p>
-      )}
+        {error && (
+          <div className="p-5 text-center">
+            <p>Couldn't load products</p>
+            <button
+              onClick={() => fetchProducts(pagination.page + 1)}
+              className="mt-3 p-3 px-5 bg-blue-950 hover:bg-blurple rounded-xl"
+            >
+              Retry
+            </button>
+          </div>
+        )}
 
-      <div ref={intersectionRef}></div>
-    </div>
+        {loading && (
+          <p className="text-center p-3">Fetching more products&hellip;</p>
+        )}
+
+        {!pagination.hasNextPage && !loading && !error && (
+          <p className="p-5 text-center">Displaying all products</p>
+        )}
+      </section>
+
+      <div ref={observerElementRef}></div>
+    </>
   )
 }
